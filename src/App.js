c151ce9dac41edd166de2370c9e5f3ced679a790
psdurbano <114499052+psdurbano@users.noreply.github.com>
2023-04-14 22:11:56 +0200

    router redux setup, navbar & item details

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
+import { useEffect } from "react";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import Home from "./pages/home/Home";
+import ItemDetails from "./pages/itemDetails/ItemDetails";
+import Checkout from "./pages/checkout/Checkout";
+import Confirmation from "./pages/checkout/Confirmation";
+import Navbar from "./pages/global/Navbar";
+import CartMenu from "./pages/global/CartMenu";
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+};
+
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Navbar />
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="item/: itemId" element={<ItemDetails />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path=" /checkout/success" element={<Confirmation />} />
+        </Routes>
+        <CartMenu />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
